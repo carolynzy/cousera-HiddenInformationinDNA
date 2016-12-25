894239387af6b8b4d88b30db2786a30d8b2e32c2
carolynzy <carolynzy@users.noreply.github.com>
2016-12-25 17:23:25 +0800

    search for most frequent K-mer

---
+++ week1_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+seq=open('dataset_2_10.txt').read()
+k=int(input('Input the k here: '))
+
+i=0
+list=[]
+unique=set()
+output={}
+value=set()
+for c in seq:
+	if i+k<len(seq)-1:
+		pattern=seq[i:i+k]
+		list.append(pattern)
+	if i+k==len(seq):
+		pattern=seq[i:]
+		list.append(pattern)
+	i+=1
+##print(list)
+for pattern in list:
+	unique.add(pattern)
+##print(unique)
+for item in unique:
+	count=list.count(item)
+	output[item]=count
+	value.add(count)
+max=max(value)
+for item in output:
+	if output[item]==max:
+		print(item)
