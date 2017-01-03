ec2de48101acbc11564bafd15a81f4995a5a34f7
carolynzy <carolynzy@users.noreply.github.com>
2017-01-03 15:51:34 +0800

    Update week1_4.py

--- week1_4.py
+++ week1_4.py
@@ -7,14 +7,10 @@ pattern=raw_input('Enter your pattern here: ')
 i=0
 len1=len(seq)
 len2=len(pattern)
-for c in seq:
-	if i+len2<=len(seq)-1:
-		pattern2=seq[i:i+len2]
-		if pattern2==pattern:
-			print(i)
-	if i+len2==len(seq):
-		pattern2=seq[i:]
-		if pattern2==pattern:
-			print(i)
+i=0
+for c in seq[0:len1-len2+1]:
+	pattern2=seq[i:i+len2]
+	if pattern2==pattern:
+		print(i)
 	i+=1
 file.close()
